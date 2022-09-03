456927b0df0973d1b700ef1d658cf21bb995ff2d
SirSabek <mmsabek99@gmail.com>
2022-09-03 17:21:50 +0200

    fixing issues

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a895b11591b9a4d7097aeff49ef79094-440121568.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
